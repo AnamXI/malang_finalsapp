bd862ae0f2935d37d0ec8b9a47367847260f65ef
AnamXI <anamggss1227@gmail.com>
2024-03-17 22:11:58 +0800

    aadawd

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,20 @@ import 'package:malang_finalsapp/home.dart';
 import 'package:malang_finalsapp/userstore.dart';
 import 'package:provider/provider.dart';
 
-void main() {
+//Firebase
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+
+
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(MultiProvider(
       providers: [
       ChangeNotifierProvider(create: (context) => BossStore()),
